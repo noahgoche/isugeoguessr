579abd26177051e0ad6f006e05a4709ff421fde1
Noah Goche <noahg21@iastate.edu>
2024-09-13 14:34:29 -0500

    Noah Exp 1 commit

---
+++ Experiments/noahExp/exp2/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,28 @@
+package coms309;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Simple Hello World Controller to display the string returned
+ *
+ * @author Vivek Bengre
+ */
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/test")
+    public String hello() {
+        return "Something different";
+    }
+    @GetMapping("/test2")
+    public String e() {
+        return "different";
+    }
+}
