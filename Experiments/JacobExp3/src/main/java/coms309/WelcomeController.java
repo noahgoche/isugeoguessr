d7fd898b4d78a233e69609d2442ec2e86c8a294f
jacobg <jaybob1225@gmail.com>
2024-09-15 14:12:19 -0500

    Exp3 Done

--- Experiments/JacobExp3/src/main/java/coms309/WelcomeController.java
+++ Experiments/JacobExp3/src/main/java/coms309/WelcomeController.java
@@ -1,11 +1,25 @@
 package coms309;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Random;
 
 @RestController
 class WelcomeController {
+
+    @GetMapping("/randomnumbers")
+    public String arm() {
+        String value = "";
+        Random random = new Random(); 
+        for (int i = 0; i < 100000; i++) {
+            int randomNumber = random.nextInt(1000000); 
+            value += randomNumber + "\n"; 
+        }
+        return value;
+    }
 
     @GetMapping("/")
     public String welcome() {
@@ -16,4 +30,5 @@ class WelcomeController {
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
     }
+
 }
