b99776c65196978a6de439872125ff7caf0a7328
Dylan Longlett <dnlonglett@gmail.com>
2024-10-25 09:26:22 -0500

    tests

--- Frontend/app/src/main/java/com/example/androidexample/PlayActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/PlayActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidexample;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class PlayActivity {
-    // h
+public class PlayActivity extends AppCompatActivity {
+
 }
