c4eee68f2b5a24f700200f09e2e3fef671a5b888
jacobg <jaybob1225@gmail.com>
2024-11-15 12:32:50 -0600

    try again

--- Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -32,7 +32,7 @@ import java.util.Map;
  * @author Jacob Garcia
  */
 public class LoginActivity extends AppCompatActivity {
-    //mini test 2
+    //mini test 3
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private Button loginButton;         // define login button variable
