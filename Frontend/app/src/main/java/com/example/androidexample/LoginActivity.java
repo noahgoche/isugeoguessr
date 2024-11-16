07faa3115699eb99060d8dd8f4265d3f62e5b935
jacobg <jaybob1225@gmail.com>
2024-11-16 11:20:35 -0600

    i hope this works

--- Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -32,7 +32,7 @@ import java.util.Map;
  * @author Jacob Garcia
  */
 public class LoginActivity extends AppCompatActivity {
-    //mini test 9
+    //mini test 10
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private Button loginButton;         // define login button variable
