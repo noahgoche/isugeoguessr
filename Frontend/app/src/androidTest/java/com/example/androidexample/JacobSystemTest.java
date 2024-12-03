7d2106550f530c307023f147e9f7362148b161d3
jacobg <jaybob1225@gmail.com>
2024-12-03 16:59:38 -0600

    Got log in test working

--- Frontend/app/src/androidTest/java/com/example/androidexample/JacobSystemTest.java
+++ Frontend/app/src/androidTest/java/com/example/androidexample/JacobSystemTest.java
@@ -1,18 +1,18 @@
 package com.example.androidexample;
 
-import android.content.Intent;
-
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class JacobSystemTest {
@@ -23,10 +23,15 @@ public class JacobSystemTest {
     }
 
     @Test
-    public void testLoginButtonClick() {
-        Espresso.onView(ViewMatchers.withId(R.id.main_login_btn)).perform(click());
+    public void testLoginNavigationAndFunctionality() {
+        Espresso.onView(withId(R.id.main_login_btn)).perform(click());
 
-        Espresso.onView(ViewMatchers.withId(R.id.login_activity_root))
+        Espresso.onView(withId(R.id.login_username_edt)).perform(typeText("jacob"));
+        Espresso.onView(withId(R.id.login_password_edt)).perform(typeText("e"));
+
+        Espresso.onView(withId(R.id.login_login_btn)).perform(click());
+
+        Espresso.onView(withId(R.id.user_home_root))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
