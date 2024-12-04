fbede09d44944fa9e296958b2e5584b2f3b52c08
Jacob Garcia <jacobgar@iastate.edu>
2024-12-03 18:50:47 -0600

    Signup and delete tests

--- Frontend/app/src/androidTest/java/com/example/androidexample/JacobSystemTest.java
+++ Frontend/app/src/androidTest/java/com/example/androidexample/JacobSystemTest.java
@@ -11,11 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class JacobSystemTest {
+
+    private static final String TEST_USERNAME = "jacob" + System.currentTimeMillis();
+    private static final String TEST_PASSWORD = "testPassword";
 
     @Before
     public void setUp() {
@@ -23,15 +27,48 @@ public class JacobSystemTest {
     }
 
     @Test
-    public void testLoginNavigationAndFunctionality() {
+    public void testSignUpAndLogin() {
+        Espresso.onView(withId(R.id.main_signup_btn)).perform(click());
+
+        Espresso.onView(withId(R.id.signup_username_edt))
+                .perform(typeText(TEST_USERNAME), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.signup_password_edt))
+                .perform(typeText(TEST_PASSWORD), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.signup_confirm_edt))
+                .perform(typeText(TEST_PASSWORD), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.signup_signup_btn)).perform(click());
+
+        Espresso.onView(withId(R.id.main_login_btn))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void testLoginWithNewAccount() {
         Espresso.onView(withId(R.id.main_login_btn)).perform(click());
 
-        Espresso.onView(withId(R.id.login_username_edt)).perform(typeText("jacob"));
-        Espresso.onView(withId(R.id.login_password_edt)).perform(typeText("e"));
-
+        Espresso.onView(withId(R.id.login_username_edt))
+                .perform(typeText(TEST_USERNAME), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.login_password_edt))
+                .perform(typeText(TEST_PASSWORD), closeSoftKeyboard());
         Espresso.onView(withId(R.id.login_login_btn)).perform(click());
 
         Espresso.onView(withId(R.id.user_home_root))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void testDeleteProfile() {
+        Espresso.onView(withId(R.id.main_login_btn)).perform(click());
+
+        Espresso.onView(withId(R.id.login_username_edt))
+                .perform(typeText(TEST_USERNAME), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.login_password_edt))
+                .perform(typeText(TEST_PASSWORD), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.login_login_btn)).perform(click());
+
+        Espresso.onView(withId(R.id.edit_profile_button)).perform(click());
+        Espresso.onView(withId(R.id.deleteProfileButton)).perform(click());
+        Espresso.onView(withId(R.id.main_login_btn))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 }
