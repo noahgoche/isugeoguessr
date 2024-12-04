56dc4c77b2b5aaa41f1ad6fe1d5610d03622aa41
Noah Goche <noahg21@iastate.edu>
2024-12-03 21:32:15 -0600

    updated delete user

--- Backend/ISU Geoguessr/src/main/java/ISUGeoguessr/UserData/UserDataController.java
+++ Backend/ISU Geoguessr/src/main/java/ISUGeoguessr/UserData/UserDataController.java
@@ -160,13 +160,14 @@ public class UserDataController {
         List<Stats> stats = userData.getStatsList();
         List<Message> messages = userData.getMessageList();
 
-        int size = Math.max(messages.size(), stats.size());
 
-        for(int i =0; i < size; i++)
-        {
-            stats.get(i).setUserData(null);
-            messages.get(i).setUserData(null);
+        for (Stats stat : stats) {
+            stat.setUserData(null);
         }
+        for(Message message: messages){
+            message.setUserData(null);
+        }
+
         userData.setStatsList(null);
         userData.setMessageList(null);
 
