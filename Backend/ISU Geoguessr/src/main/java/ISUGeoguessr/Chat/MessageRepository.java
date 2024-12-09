564b7ee9eb3c76ab256fa6659f8bc1cf3167ed26
Noah Goche <noahg21@iastate.edu>
2024-12-09 16:19:38 -0600

    fixed delete

--- Backend/ISU Geoguessr/src/main/java/ISUGeoguessr/Chat/MessageRepository.java
+++ Backend/ISU Geoguessr/src/main/java/ISUGeoguessr/Chat/MessageRepository.java
@@ -1,8 +1,12 @@
 package ISUGeoguessr.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long>{
+
+    @Transactional
+    void deleteById(int msgId);
 
 }
